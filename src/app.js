33a2093c454812051a9ad1ce8fd6559f4fd9b6ad
isabel-nett <barnettisabel@gmail.com>
2021-07-05 20:49:00 -0500

    Cleaned up & added functionality

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+let searchBox = document.getElementById("city-input");
+let searchButton = document.getElementById("search-button");
+let temperature = document.getElementById("temperature");
+let cityName = document.getElementById("city-output");
+let currentLocation = document.getElementById("use-current-location");
+let weatherDescription = document.getElementById("weather-description");
+let humidityElement = document.getElementById("humidity");
+let windElement = document.getElementById("wind");
+let iconElement = document.getElementById("weather-icon");
+let farenheitLink = document.getElementById("farenheit");
+let celciusLink = document.getElementById("celcius");
+
 function formatDate() {
   let now = new Date();
   let daysOfWeek = [
@@ -12,26 +24,21 @@ function formatDate() {
 
   let day = daysOfWeek[now.getDay()];
   let hour = now.getHours();
+  if (hour < 10) {
+      hour = `0${hour}`;
+  }
   let minutes = now.getMinutes();
+  if (minutes < 10) {
+      minutes = `0${minutes}`;
+  };
 
   todayDate = document.getElementById("today-date");
   todayDate.innerHTML=(day + " " + hour + ":" + minutes);
 }
 
-formatDate();
-
-
-let searchBox = document.getElementById("city-input");
-let searchButton = document.getElementById("search-button");
-let temperature = document.getElementById("temperature");
-let cityName = document.getElementById("city-output");
-let currentLocation = document.getElementById("use-current-location");
-let weatherDescription = document.getElementById("weather-description")
-let humidityElement = document.getElementById("humidity")
-let windElement = document.getElementById("wind")
-
-function searchCity() {
-  let city = searchBox.value  
+function searchCity(event) {
+  event.preventDefault();
+  let city = searchBox.value;
   let apiKey = "25174e4a6986b7dcdd6d3651f3f8973a";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
@@ -39,7 +46,6 @@ function searchCity() {
 }
 
 function searchCurrent(position) {
-  console.log("searchPosition is working")
   let apiKey = "25174e4a6986b7dcdd6d3651f3f8973a";
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
@@ -49,14 +55,38 @@ function searchCurrent(position) {
 }
 
 function showWeather(response) {
-    console.log(response);
-  let currentTemp = Math.round(response.data.main.temp);
-  temperature.innerHTML = (currentTemp);
+  celciusTemp = Math.round(response.data.main.temp);
+  temperature.innerHTML = (celciusTemp);
   cityName.innerHTML = response.data.name;
-  weatherDescription.innerHTML = response.data.weather[0].main;
+  weatherDescription.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = reponse.data.main.wind;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function displayFTemp(event) {
+    event.preventDefault();
+    celciusLink.classList.remove("active");
+    farenheitLink.classList.add("active");
+    let farenheitTemp = (celciusTemp * 9) / 5 + 32;
+    temperature.innerHTML = Math.round(farenheitTemp);
+}
+
+function displayCTemp(event) {
+    event.preventDefault();
+    celciusLink.classList.add("active");
+    farenheitLink.classList.remove("active");
+    temperature.innerHTML = Math.round(celciusTemp);
+}
+
+let celciusTemp = null;
+
+document.querySelector('#farenheit').addEventListener("click", displayFTemp);
+document.querySelector('#celcius').addEventListener("click", displayCTemp);
 document.querySelector('#search-button').addEventListener("click", searchCity);
 document.querySelector('#use-current-location').addEventListener("click", navigator.geolocation.getCurrentPosition(searchCurrent));
+formatDate();
