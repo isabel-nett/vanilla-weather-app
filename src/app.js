a4c5e72b7a4083d6a88318549ce7f4462e9552be
isabel-nett <barnettisabel@gmail.com>
2021-07-12 08:01:07 -0500

    All APIs are functioning

--- src/app.js
+++ src/app.js
@@ -36,6 +36,59 @@ function formatDate() {
   todayDate.innerHTML=(day + " " + hour + ":" + minutes);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#five-day-forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+   `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "25174e4a6986b7dcdd6d3651f3f8973a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
 function searchCity(event) {
   event.preventDefault();
   let city = searchBox.value;
@@ -66,6 +119,8 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function displayFTemp(event) {
@@ -91,8 +146,6 @@ document.querySelector('#search-button').addEventListener("click", searchCity);
 document.querySelector('#use-current-location').addEventListener("click", navigator.geolocation.getCurrentPosition(searchCurrent));
 formatDate();
 
-
-
 let quoteText = document.getElementById("quote-text");
 let quoteAuthor = document.getElementById("quote-author");
 
@@ -102,9 +155,9 @@ function getDailyQuote() {
 }
 
 function showDailyQuote(response) {
-  console.log(response.contents.quotes.quote)
-  let quoteContent = (response.contents.quotes.quote);
-  let quoteBy = (response.contents.quotes.author);
+  console.log(response)
+  let quoteContent = (response.data.contents.quotes[0].quote);
+  let quoteBy = (response.data.contents.quotes[0].author);
   quoteText.innerHTML = `${quoteContent}`;
   quoteAuthor.innerHTML = `-${quoteBy}`;
 }
